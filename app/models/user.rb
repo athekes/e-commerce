7b9d0a5a294244ac6dd9a5bd92e3ad27415ab434
Gustavo <lgx.0097@gmail.com>
2025-01-28 16:30:02 -0300

    Add length validation for CPF

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          authentication_keys: [:login]
 
-  validates :cpf, presence: true, uniqueness: { case_sensitive: false }
+  validates :cpf, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 11 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }
 
   has_one :cart, dependent: :destroy
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe User, type: :model do
     subject { User.new(cpf: '12345678900', email: 'test@example.com') }
 
     it { should validate_presence_of(:cpf) }
+    it { should validate_length_of(:cpf).is_at_most(11) }
     it { should validate_uniqueness_of(:cpf).case_insensitive }
-
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email).case_insensitive }
   end
